261d5b19d277c7af76f544288a5a2d2bc0de2eb3
Romeu Beato <romeufbeato@gmail.com>
2021-05-04 12:21:50 +0100

    pre publishing

--- bundle.js
+++ bundle.js
@@ -2,11 +2,11 @@
 const emailjs = require('emailjs-com')
 
 const submitForm = (e) => {
-      e.prevenDefault();
-
+      e.preventDefault();
       emailjs.sendForm('service_mfz0aqc', 'template_lkkto0p', 'contact-form', 'user_8kKA2rSFilIqUQtJointB')
-            .then(result => alert('Your message has been sent. I will be contacting you soon. Thank you!'))
-            .catch(err => alert('Oops! Please try again!'))
+            .then(result => alert('Your message has been sent. I will be in contact with you soon.Thank you!'))
+            .catch(err => alert('Oops! Please try again.'))
+
 }
 
 document.querySelector('.submitButton').addEventListener('click', submitForm)
